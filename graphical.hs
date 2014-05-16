4b708b84bbb8a068dd3bc9e81b840272d291d363
Kamila Souckova <kamila@vesmir.sk>
2014-05-16 11:27:08 +0200

    make the animation prettier -- change constants

--- graphical.hs
+++ graphical.hs
@@ -5,8 +5,8 @@ import Graphics.Gloss.Raster.Field
 palette = map (\v -> makeColor 0 (v^2) 0.1 0) [0, 0.02 .. 1]
 
 animate :: Fractal -> Float -> (Point -> Color)
-animate frac t (re,im) = mkimage frac palette (re/st :+ im/st)
-                         where st = sqrt t/3
+animate frac t (re,im) = mkimage frac palette ((re/sc - 0.108) :+ (im/sc - 0.1))
+                         where sc = (1.1**t)/2
 
 window = InWindow "Magic!" (800, 600) (100, 100)
 
